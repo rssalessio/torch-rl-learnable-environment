a66ad11f3998d633730395cb4105a8cbe5cd9de3
Alessio Russo <russo.alessio@outlook.com>
2022-07-22 14:44:06 +0200

    update init file

--- learnable_environment/__init__.py
+++ learnable_environment/__init__.py
@@ -2,7 +2,7 @@ from .cartpole import CartPoleLearnableEnvironment
 from .mountaincar import MountainCarLearnableEnvironment
 from .learnable_environment import LearnableEnvironment
 
-__version__       = '0.0.3'
+__version__       = '0.0.4'
 __author__        = 'Alessio Russo'
 __contributors__  = []
-__date__          = '21.07.2022'
+__date__          = '22.07.2022'
