fix log likelihood computation

examples/example.py
@@ -16,7 +16,7 @@ env = gym.make(ENV_NAME)
 # Parameters
 num_samples = 3000
 batch_size = 64
-n_models = 5
+n_models = 15
 state_dim = np.prod(env.observation_space.shape)
 action_dim = 1 if isinstance(env.action_space, gym.spaces.Discrete) else np.prod(env.action_space.shape)
 reward_dim = 1

examples/example_kl_div.py
@@ -65,5 +65,5 @@ print(f'KL Divergence between the two models: {mean_kl.item()} +- {1.96 * std_kl
 mean_kl, std_kl = env1.compute_kl_divergence_over_batch(state[-num_evaluation:], action[-num_evaluation:], env2, num_avg_over_ensembles=10)
 print(f'KL Divergence between the two models: {mean_kl.item()} +- {1.96 * std_kl.item() / np.sqrt(10)} (95% confidence)')
 
-log_probs = env1.compute_log_prob_batch(state[-num_evaluation:], action[-num_evaluation:])
-print(f'Log probabilities: {log_probs}')
+log_probs = env1.compute_log_likelihood_batch(state[-num_evaluation:], action[-num_evaluation:], next_state[-num_evaluation:])
+print(f'Max Log likelihood: {log_probs.max()}')

learnable_environment/ensemble_model/gaussian_ensemble_model.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 from functools import reduce
 from typing import Dict, List, Tuple, Type
 import numpy as np
+from sklearn.covariance import log_likelihood
 import torch
 from sklearn.preprocessing import StandardScaler
 from learnable_environment.ensemble_model.ensemble_model import EnsembleModel
@@ -153,20 +154,24 @@ class GaussianEnsembleModel(EnsembleModel):
 
         return training_losses, test_losses
 
-    def compute_log_prob_batch(self, inputs: NDArray, X: NDArray) -> torch.Tensor:
+    def compute_log_likelihood_batch(self, inputs: NDArray, X: NDArray) -> torch.Tensor:
+        assert X.shape[-1] <= inputs.shape[-1]
         mean, var = self._predict(inputs)
 
-        k = inputs.shape[-1]
+        k = X.shape[-1]        
+
+        mean = mean[:,:,:k]
+        var = var[:,:,:k]
         ## [ num_networks, batch_size ]
-        log_prob = -1 / 2 * (k * np.log(2 * np.pi) + torch.log(var).sum(-1) + (torch.pow(torch.from_numpy(X) - mean, 2) / var).sum(-1))
-        
+        log_likelihood = -1 / 2 * (k * np.log(2 * np.pi) + torch.log(var).sum(-1) + (torch.pow(torch.from_numpy(X) - mean, 2) / var).sum(-1))
+
         # ## [ batch_size ]
         # prob = torch.exp(log_prob).sum(0)
 
         # ## [ batch_size ]
         # log_prob = torch.log(prob)
 
-        return log_prob.mean(0)
+        return log_likelihood.mean(0)
 
     def compute_kl_divergence_over_batch(self,
             inputs: NDArray,

learnable_environment/learnable_environment.py
@@ -123,12 +123,15 @@ class LearnableEnvironment(gym.Env):
     def close(self):
         pass
 
-    def compute_log_prob_batch(self, states: NDArray, actions: NDArray, next_states: NDArray, rewards: Optional[NDArray] = None) -> torch.Tensor:
+    def compute_log_likelihood_batch(self, states: NDArray, actions: NDArray, next_states: NDArray, rewards: Optional[NDArray] = None) -> torch.Tensor:
         if isinstance(self._model, GaussianEnsembleModel):
             _action = np.array(actions)
             actions_reshaped = np.expand_dims(_action, axis=tuple(range(len(_action.shape), len(states.shape))))
             inputs = np.concatenate((states, actions_reshaped), axis=-1)
-            return self.model.compute_log_prob_batch(inputs, next_states)
+    
+            outputs = np.concatenate((next_states, rewards), axis=-1) if rewards is not None else next_states
+
+            return self.model.compute_log_likelihood_batch(inputs, outputs)
         else:
             raise Exception('Not implemented')
 